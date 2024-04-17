d2b5f09887ccf838a59fcf191d7ea406ef33cb7d
Gonçalo <70868465+Saleca@users.noreply.github.com>
2024-04-17 22:03:22 +0100

    init

---
+++ DebugFeatures/MainPage.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace DebugFeatures
+{
+	public partial class MainPage : ContentPage
+	{
+		public MainPage()
+		{
+			InitializeComponent();
+		}
+
+		private void ButtonClicked(object sender, EventArgs e) => Btn.Text = "Pressed";
+	}
+}
---
+++ DebugFeatures/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace DebugFeatures
+{
+	[Register("AppDelegate")]
+	public class AppDelegate : MauiUIApplicationDelegate
+	{
+		protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+	}
+}
